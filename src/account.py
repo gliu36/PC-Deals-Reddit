eb310f007e70d87bcdc61ec0005239a94ccba6cf
Gerry <gerryliu36@gmail.com>
2018-07-29 12:34:34 -0500

    removed venv

--- src/account.py
+++
@@ -1,8 +0,0 @@
-import praw
-
-
-reddit = praw.Reddit(client_id='F5sxyU5zS3O-nQ',
-                     client_secret='-1I-7OPD4BMT0M-aeOuj1qVKMUg',
-                     username='Fanwn',
-                     password='',
-                     user_agent='')
