21bc4284e5be6822380b96436cd943435d29cccb
gliu36 <gerryliu36@gmail.com>
2020-01-23 01:26:24 -0500

    mech keeb hunt

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,10 @@
 import praw
 from win10toast import ToastNotifier
 import time
+import webbrowser
+
+SUBREDDIT = 'mechmarket'
+SCRAPE_TERMS = ['holy', 'panda', 'pandas', 'satisfaction']
 
 reddit = praw.Reddit(client_id='j50P1dFZAmUU_g',
                      client_secret='OVOADtNSpXda1oWKGFD6iGrRTek',
@@ -11,10 +15,11 @@ reddit = praw.Reddit(client_id='j50P1dFZAmUU_g',
 notification = ToastNotifier()
 start_time = time.time()
 
-for post in reddit.subreddit('hardwareswap').stream.submissions():
+for post in reddit.subreddit(SUBREDDIT).stream.submissions():
     if post.created_utc > start_time:
-        if "8600k" in post.title:
+        if any(term in post.title.lower() for term in SCRAPE_TERMS):
             print(post.title, post.url)
-            notification.show_toast(post.title, post.url, duration=4)
+            notification.show_toast(post.title, post.url, duration=3)
+            webbrowser.open_new_tab(post.url)
             with open('posts.txt', 'a+', encoding="utf-8") as file:
                 file.write(post.title + ", Link: " + post.url + "\n")
